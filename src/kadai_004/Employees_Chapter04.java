b2800e3463df8ac92f5e939105f13abaef6fe41d
y.m7236 <y.m7236@outlook.jp>
2023-06-08 20:54:28 +0900

    データベース課題04の提出

---
+++ src/kadai_004/Employees_Chapter04.java
@@ -0,0 +1,53 @@
+package kadai_004;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Employees_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Connection con = null;
+		Statement statement = null;
+		
+		try {
+			//データベースに接続
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost/challenge_java",
+					"root",
+					"test"//本来はファイルから読み込むか、入力式にする
+			);
+			
+			System.out.println("データベース接続成功");
+			System.out.println(con);
+			
+			//SQLクエリを準備
+			statement = con.createStatement();
+			String sql = """
+					CREATE TABLE employees (
+						id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+						name VARCHAR(60) NOT NULL,
+						email VARCHAR(255) NOT NULL,
+						age INT(11),
+						address VARCHAR(255)
+						);
+					""";
+			//SQLクエリを実行（DBMSに送信）
+			int rowCnt = statement.executeUpdate(sql);
+			System.out.println("社員テーブルを作成しました：更新レコード数=" + rowCnt );
+		}catch(SQLException e) {
+			System.out.println("データベース接続失敗：" + e.getMessage());
+		}finally {
+			if( statement != null) {
+				try { statement.close();} catch(SQLException ignore) {}
+			}
+			if( con != null) {
+				try { con.close(); } catch(SQLException ignore) {}
+			}
+		}
+	}
+	
+
+}
