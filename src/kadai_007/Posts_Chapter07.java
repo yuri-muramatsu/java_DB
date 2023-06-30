0ed9feba0a69c40ad988b83aaca5cfb5e86ed7d6
y.m7236 <y.m7236@outlook.jp>
2023-07-01 08:19:58 +0900

    コードの整理

--- src/kadai_007/Posts_Chapter07.java
+++ src/kadai_007/Posts_Chapter07.java
@@ -1,72 +1,84 @@
 package kadai_007;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
 
 public class Posts_Chapter07 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+	public static void main(final String[] args) {
 		Connection con = null;
-		Statement statement = null;
-		
+		PreparedStatement statement1 = null;
+
 		try {
-			//データベースに接続
-			con = DriverManager.getConnection(
-					"jdbc:mysql://localhost/challenge_java",
-					"root",
-					"test"//本来はファイルから読み込むか、入力式にする
-			);
-			
+			// データベースに接続
+			con = DriverManager.getConnection("jdbc:mysql://localhost/challenge_java", "root", "test");
 			System.out.println("データベース接続成功：" + con);
-			
-			//SQLクエリを準備
-			statement = con.createStatement();
-			
-			//投稿データを追加
-			String sql = """
+			// 投稿データを追加
+			final String sql1 = """
 					INSERT INTO posts (user_id, posted_at, post_content, likes) VALUES
-					(1003, '2023-02-08', '昨日の夜は徹夜でした・・', 13),
-					(1002, '2023-02-08', 'お疲れ様です！', 12),
-					(1003, '2023-02-09', '今日も頑張ります！', 18),
-					(1001, '2023-02-09', '無理は禁物ですよ！', 17),
-					(1002, '2023-02-10', '明日から連休ですね！', 20);
+					(?, ?, ?, ?),(?, ?, ?, ?),(?, ?, ?, ?),(?, ?, ?, ?),(?, ?, ?, ?);
 					""";
-			
-			//SQLクエリを実行（DBMSに送信）
-			int rowCnt = statement.executeUpdate(sql);
+			// SQLクエリを準備
+			statement1 = con.prepareStatement(sql1);
+			// 追加する投稿データのセット
+			statement1.setInt(1, 1003);
+			statement1.setDate(2, new Date(System.currentTimeMillis()));
+			statement1.setString(3, "昨日の夜は徹夜でした・・");
+			statement1.setInt(4, 13);
+			statement1.setInt(5, 1002);
+			statement1.setString(6, "2023-02-08");
+			statement1.setString(7, "お疲れ様です！");
+			statement1.setInt(8, 12);
+			statement1.setInt(9, 1003);
+			statement1.setString(10, "2023-02-09");
+			statement1.setString(11, "今日も頑張ります！");
+			statement1.setInt(12, 18);
+			statement1.setInt(13, 1001);
+			statement1.setString(14, "2023-02-09");
+			statement1.setString(15, "無理は禁物ですよ！");
+			statement1.setInt(16, 17);
+			statement1.setInt(17, 1002);
+			statement1.setString(18, "2023-02-10");
+			statement1.setString(19, "明日から連休ですね！");
+			statement1.setInt(20, 20);
+			// SQLクエリを実行（DBMSに送信）
+			final int rowCnt = statement1.executeUpdate();
 			System.out.println("レコード追加を実行します。");
-			System.out.println( rowCnt + "件のレコードが追加されました。");
-			
-			
-			//データの検索 表示形式「N件目：投稿日時=〇／投稿内容=△／いいね数=□」
-			String sql1 = "SELECT * FROM posts WHERE user_id = 1002;";
+			System.out.println(rowCnt + "件のレコードが追加されました。");
+
+			// データの検索
+			final String sql2 = "SELECT * FROM posts WHERE user_id=?;";
+			final PreparedStatement statement2 = con.prepareStatement(sql2);
+			// 検索するユーザーIDのセット
+			statement2.setInt(1, 1002);
 			System.out.println("ユーザーIDが1002のレコードを検索しました。");
-			
-			ResultSet result = statement.executeQuery(sql1);	
-			
-			while(result.next()) {
-				//int id = result.getInt("user_id");
-				Date postedAt = result.getDate("posted_at");
-				String content = result.getString("post_content");
-				int likes = result.getInt("likes");
-				System.out.println
-					(result.getRow() + "件目：投稿日時＝" + postedAt + "/投稿内容＝ " + content + "/いいね数＝ " +likes);
-			};
-			
-		}catch(SQLException e) {
+			// SQLクエリを実行（DBMSに送信）
+			final ResultSet result = statement2.executeQuery();
+
+			while (result.next()) {
+				final Date postedAt = result.getDate("posted_at");
+				final String content = result.getString("post_content");
+				final int likes = result.getInt("likes");
+				System.out.println(result.getRow() + "件目：投稿日時＝" + postedAt + "/投稿内容＝ " + content + "/いいね数＝ " + likes);
+			}
+		} catch (final SQLException e) {
 			System.out.println("エラー発生：" + e.getMessage());
-		}finally {
-			if(statement != null) {
-				try {statement.close();} catch(SQLException ignore) {}
+		} finally {
+			if (statement1 != null) {
+				try {
+					statement1.close();
+				} catch (final Exception ignore) {
+				}
 			}
 			if (con != null) {
-				try {con.close();}catch(SQLException ignore) {}
+				try {
+					con.close();
+				} catch (final Exception ignore) {
+				}
 			}
 		}
 	}
--- src/kadai_010/Scores_Chapter10.java
+++ src/kadai_010/Scores_Chapter10.java
@@ -2,74 +2,66 @@ package kadai_010;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-/*データベース接続成功：com.mysql.cj.jdbc.ConnectionImpl@xxxxxxxx
-レコード更新を実行します
-1件のレコードが更新されました
-数学・英語の点数が高い順に並べ替えました
-1件目：生徒ID=5／氏名=武者小路勇気／数学=95／英語=80
-2件目：生徒ID=2／氏名=刀沢彩香／数学=85／英語=70
-3件目：生徒ID=4／氏名=武士山美咲／数学=75／英語=95
-4件目：生徒ID=3／氏名=戦国広志／数学=75／英語=85
-5件目：生徒ID=1／氏名=侍健太／数学=65／英語=90*/
 
 public class Scores_Chapter10 {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public static void main(final String[] args) {
 		Connection con = null;
-		Statement statement = null;
-		
+		PreparedStatement statement = null;
+
 		try {
-			//データベースに接続
-			con = DriverManager.getConnection(
-					"jdbc:mysql://localhost/challenge_java",
-					"root",
-					"test"
-					);
-			
+			// データベースに接続
+			con = DriverManager.getConnection("jdbc:mysql://localhost/challenge_java", "root", "test");
 			System.out.println("データベース接続成功:" + con);
-			
-			//SQLクエリを準備
-			statement = con.createStatement();
-			String sql = "UPDATE scores1 SET score_math = 95 , score_english = 80 WHERE id = '5';";
-			
-			//点数データを更新　SQLクエリを実行（DBMSに送信）
+
+			// SQLクエリを準備
+			final String sql1 = "UPDATE scores1 SET score_math=?, score_english=? WHERE id=?;";
+			statement = con.prepareStatement(sql1);
+			// 更新するデータのセット
+			statement.setInt(1, 65);
+			statement.setInt(2, 45);
+			statement.setInt(3, 5);
+
+			// 点数データを更新 SQLクエリを実行（DBMSに送信）
 			System.out.println("レコード更新を実行します。");
-			int rowCnt = statement.executeUpdate(sql);
+			final int rowCnt = statement.executeUpdate();
 			System.out.println(rowCnt + "件のレコードが更新されました。");
-			
-			//点数順に並べる
-			String sql1 = "SELECT * FROM scores1 ORDER BY score_math DESC, score_english DESC;";
-			
-			//SQLクエリを実行（DBMSに送信）
+
+			// 点数順に並べる
+			final String sql2 = "SELECT * FROM scores1 ORDER BY score_math DESC, score_english DESC;";
+			// SQLクエリを実行（DBMSに送信）
 			System.out.println("数学・英語の点数が高い順に並べ替えました");
-			ResultSet result = statement.executeQuery(sql1);
-			
-			//SQLクエリの実行結果を抽出
-			while(result.next()) {
-				int id = result.getInt("id");
-				String name = result.getString("name");
-				int scoreMath = result.getInt("score_math");
-				int scoreEnglish = result.getInt("score_english");
-				System.out.println
-					(result.getRow() + "件目：生徒ID＝" + id + "/氏名＝" + name + "/数学＝" +scoreMath + "/英語＝" + scoreEnglish);
-			};
-			
-		} catch(SQLException e) {
-			System.out.println("エラー発生：" + e.getMessage());
-		}finally {
-			if( statement != null ) {
-				try { statement.close();} catch (SQLException ignore) {}
+			final ResultSet result = statement.executeQuery(sql2);
+
+			// SQLクエリの実行結果を抽出
+			while (result.next()) {
+				final int id = result.getInt("id");
+				final String name = result.getString("name");
+				final int scoreMath = result.getInt("score_math");
+				final int scoreEnglish = result.getInt("score_english");
+
+				System.out.println(
+						result.getRow() + "件目：生徒ID＝" + id + "/氏名＝" + name + "/数学＝" + scoreMath + "/英語＝" + scoreEnglish);
 			}
-			if ( con != null ) {
-				try { con.close();} catch(SQLException ignore) {}
+		} catch (final SQLException e) {
+			System.out.println("エラー発生：" + e.getMessage());
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (final Exception ignore) {
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (final Exception ignore) {
+				}
 			}
 		}
 	}
-
 
 }
